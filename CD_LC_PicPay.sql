5456914e524f7c91fa55a9a366a3eef3e4c8bfc8
luanpedro0014 <42987466+luanpedro0014@users.noreply.github.com>
2021-05-12 16:01:25 -0300

    Add files via upload

---
+++ CD_LC_PicPay.sql
@@ -0,0 +1,31 @@
+/****** Object:  Table [dbo].[CD_LC_PicPay]    Script Date: 12/05/2021 15:20:01 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[CD_LC_PicPay](
+	[Nome] [text] NULL,
+	[Email] [text] NULL,
+	[Data Transação] [text] NULL,
+	[Data Último Status] [text] NULL,
+	[Tipo] [text] NULL,
+	[Valor] [text] NULL,
+	[Operação] [text] NULL,
+	[Método] [text] NULL,
+	[Pedido] [text] NULL,
+	[Status] [text] NULL,
+	[Tid] [text] NULL,
+	[Uuid] [text] NULL,
+	[Saldo] [text] NULL,
+	[Empresa] [text] NULL,
+	[Liga] [text] NULL,
+	[Valor da Liga] [text] NULL,
+	[Times] [text] NULL,
+	[Valor Times] [text] NULL,
+	[Transação Relacionada] [text] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
