5fa0300ee59dab93b54454de9fa3c40b2322cf8c
luanpedro0014 <42987466+luanpedro0014@users.noreply.github.com>
2021-05-17 09:34:23 -0300

    Add files via upload

---
+++ TCS_MP_Paymee.sql
@@ -0,0 +1,32 @@
+/****** Object:  Table [dbo].[TCS_MP_Paymee]    Script Date: 17/05/2021 09:15:31 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TCS_MP_Paymee](
+	[ID] [text] NULL,
+	[Nome] [text] NULL,
+	[Email] [text] NULL,
+	[Data Transação] [text] NULL,
+	[Data Último Status] [text] NULL,
+	[Status] [text] NULL,
+	[Tipo] [text] NULL,
+	[Valor] [text] NULL,
+	[Operação] [text] NULL,
+	[Método] [text] NULL,
+	[Pedido] [text] NULL,
+	[Tid] [text] NULL,
+	[Uuid] [text] NULL,
+	[Saldo] [text] NULL,
+	[Empresa] [text] NULL,
+	[Liga] [text] NULL,
+	[Valor da Liga] [text] NULL,
+	[Times] [text] NULL,
+	[Valor Times] [text] NULL,
+	[Transação Relacionada] [text] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
